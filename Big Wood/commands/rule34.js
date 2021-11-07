d36c8a6ee5d8ec2af2fb842d2e66196f793b4590
Rendra Kusuma <82817358+MainLah@users.noreply.github.com>
2021-11-07 09:19:40 +0700

    Add files via upload

--- Big Wood/commands/rule34.js
+++ Big Wood/commands/rule34.js
@@ -1,14 +1,17 @@
 const fetch = require('node-fetch');
-const path = require('path');
 
 module.exports = {
     category: 'Adult',
     description: 'Sends random rule 34 picture',
+    expectedArgs: '<tags>',
+    minArgs: 0,
 
-    callback: async ({ message }) => {
-        const randomPic = await fetch('https://rule34.xxx/index.php?page=dapi&s=post&q=index&json=1&limit=1', {method: 'GET'})
+    callback: async ({ message, args }) => {
+        
+        let randomPic = await fetch(`https://rule34.xxx/index.php?page=dapi&s=post&q=index&json=1&limit=10&tags=${args.join('')}`, {method: 'GET'})
+        .then(response => response.json())
 
-        message.reply({files: [{attachment: randomPic.preview_url}]})
+        message.channel.send({files: [{attachment: randomPic[Math.floor(Math.random() * 10)].file_url}]})
         return
     }
 }
