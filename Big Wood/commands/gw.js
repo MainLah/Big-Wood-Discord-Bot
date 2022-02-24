2a6d7bf496c1e4b1ff1319e70edebf40b6c19c11
Rendra Kusuma <82817358+MainLah@users.noreply.github.com>
2022-02-24 08:37:36 +0700

    Update gw.js

--- Big Wood/commands/gw.js
+++ Big Wood/commands/gw.js
@@ -1,58 +1,71 @@
-const fetch = require('node-fetch');
-const { MessageEmbed } = require('discord.js');
-const DiscordJS = require('discord.js');
+const fetch = require("node-fetch");
+const { MessageEmbed } = require("discord.js");
+const DiscordJS = require("discord.js");
+const { minArgs } = require("./r34");
 
 module.exports = {
-    category: 'Fun',
-    description: 'Guess the missing word',
-    
-    slash: 'both',
-    testOnly: false,
-    
-    callback: async (message) => {
-        //takes a random quote from zenquote api, chooses a random word to be guessed
-        const quote = await fetch("https://zenquotes.io/api/random", {method: 'GET'}).then(response => response.json());
-        const array = quote[0]["q"].split(' ')
-        const randomWord = array[Math.floor(Math.random() * array.length)].replace('.', '')
-        const newMessage = quote[0]["q"].replace(randomWord, '_'.repeat(randomWord.length))
-        
-        const question = new MessageEmbed()
-        .setColor('#6bb9f0')
-        .setTitle('Guess the missing word')
-        .setDescription(newMessage)
-        if (randomWord.length > 1) {
-            question.addField('Hint:', `First letter is "${randomWord[0]}" and it's ${randomWord.length} letters long`)
-        } else {
-            question.addField('Hint:', 'The word only has 1 letter')
-        }
-        message.channel.send({embeds: [question]})
+  category: "Fun",
+  description: "Guess the missing word",
+  cooldown: "5s",
 
-        console.log(randomWord)
-        
-        //checking user's message if correct or wrong
-        const filter = (m) => {
-            return m.author.id === message.author.id
-        }
+  slash: "both",
+  testOnly: false,
 
-        const collector = new DiscordJS.MessageCollector(message.channel, filter, {
-            max: 1,
-            time: 5000
-        })
+  callback: async (message) => {
+    const quote = await fetch("https://zenquotes.io/api/random", {
+      method: "GET",
+    }).then((response) => response.json());
 
-        const timer = setInterval(() => {
-            message.channel.send(`Time's out! The answer is "${randomWord.toUpperCase()}"`)
-            collector.stop()
-            clearInterval(timer)
-        }, 15000)
+    const array = quote[0]["q"].split(" ");
 
-        collector.on('collect', m => {
-            if (m.content.toLowerCase() === randomWord.toLowerCase()) {
-                m.reply('Correct!')
-                collector.stop()
-                clearInterval(timer)
-                return
-            }
-        })
-    },
-}
+    const randomWord = array[Math.floor(Math.random() * array.length)].replace(
+      /[.;,]/g,
+      ""
+    );
 
+    const newMessage = quote[0]["q"].replace(
+      randomWord,
+      "_".repeat(randomWord.length)
+    );
+
+    const question = new MessageEmbed()
+      .setColor("#6bb9f0")
+      .setTitle("Guess the missing word")
+      .setDescription(newMessage);
+    if (randomWord.length > 1) {
+      question.addField(
+        "Hint:",
+        `First letter is "${randomWord[0]}" and it's ${randomWord.length} letters long`
+      );
+    } else {
+      question.addField("Hint:", "The word only has 1 letter");
+    }
+    message.channel.send({ embeds: [question] });
+
+    console.log(randomWord);
+
+    const filter = (m) => {
+      return m.author.id === message.author.id;
+    };
+
+    const collector = new DiscordJS.MessageCollector(message.channel, filter, {
+      max: 1,
+      time: 5000,
+    });
+
+    setTimeout(() => {
+      message.channel.send(
+        `Time's out! The answer is "${randomWord.toUpperCase()}"`
+      );
+      collector.stop("Time's out");
+    }, 25000);
+
+    collector.on("collect", (m) => {
+      if (m.content.toLowerCase() === randomWord.toLowerCase()) {
+        m.reply("Correct!");
+        collector.stop();
+        return;
+      }
+    });
+  },
+};
